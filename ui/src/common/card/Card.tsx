3ffe947e61bc0fa7070e1256c2d80e325a408d89
Mario Kaufmann <mario.kaufmann@zuehlke.com>
2023-05-10 16:46:42 +0200

    Run prettier on UI

--- ui/src/common/card/Card.tsx
+++ ui/src/common/card/Card.tsx
@@ -1,6 +1,10 @@
 import { Component, ParentComponent } from 'solid-js';
 import styles from './Card.module.css';
 const Card: ParentComponent = (props) => {
-  return <div class={styles.Card} data-testid="card">{props.children}</div>;
+  return (
+    <div class={styles.Card} data-testid="card">
+      {props.children}
+    </div>
+  );
 };
 export default Card;
