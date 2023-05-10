b58bcc5262a635d171dfbff1e5df047987ec2244
Mario Kaufmann <mario.kaufmann@zuehlke.com>
2023-05-10 16:43:02 +0200

    Prettier for mock and e2e

--- tests/e2e/tests/repositories.spec.ts
+++ tests/e2e/tests/repositories.spec.ts
@@ -1,15 +1,16 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('show repositories on dashboard', async ({page}) => {
-    await page.goto('/');
+test("show repositories on dashboard", async ({ page }) => {
+  await page.goto("/");
 
-    const cards = await page.getByTestId('card');
-    await expect(cards).toHaveCount(8);
+  const cards = await page.getByTestId("card");
+  await expect(cards).toHaveCount(8);
 
-    const firstCard = cards
-        .filter({has: page.getByText('EXP/example-frontend')});
-    await expect(firstCard).toBeVisible();
+  const firstCard = cards.filter({
+    has: page.getByText("EXP/example-frontend"),
+  });
+  await expect(firstCard).toBeVisible();
 
-    await expect(firstCard.getByText("main")).toBeVisible();
-    await expect(firstCard.getByText("feature/matrix-index")).toBeVisible();
+  await expect(firstCard.getByText("main")).toBeVisible();
+  await expect(firstCard.getByText("feature/matrix-index")).toBeVisible();
 });
