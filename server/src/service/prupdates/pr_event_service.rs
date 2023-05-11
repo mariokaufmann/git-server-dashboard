8db2e57b292f036371f1c6d9cc015e6f58063ceb
geti <tiziano.gemma@zuehlke.com>
2023-05-11 14:04:43 +0200

    refactor get_pr_updates

--- server/src/service/prupdates/pr_event_service.rs
+++ server/src/service/prupdates/pr_event_service.rs
@@ -1,9 +1,12 @@
-use crate::adapter::db::prupdates::PullRequestEventRepository;
-use crate::service::prupdates::aggregate::aggregate_events;
-use crate::service::prupdates::model::{PullRequestEvent, PullRequestUpdate};
+use std::cmp::Reverse;
+use std::collections::HashMap;
+
 use anyhow::Context;
 use chrono::{DateTime, Utc};
-use std::collections::HashMap;
+
+use crate::adapter::db::prupdates::PullRequestEventRepository;
+use crate::service::prupdates::aggregate::aggregate_events;
+use crate::service::prupdates::model::{PullRequestEvent, PullRequestTimestamp, PullRequestUpdate};
 
 #[derive(Clone)]
 pub struct PullRequestUpdateService {
@@ -26,39 +29,65 @@ impl PullRequestUpdateService {
         last_seen_timestamps: HashMap<String, DateTime<Utc>>,
     ) -> anyhow::Result<Vec<PullRequestUpdate>> {
         let events = self.pr_event_repository.get_events().await?;
+        let filtered_events = Self::filter_unseen_events(last_seen_timestamps, events);
+        let grouped_events = Self::group_events_by_pr(filtered_events);
+        let sorted_events = Self::sort_by_latest_timestamp(grouped_events);
+        let updates = Self::aggregate_events_into_updates(sorted_events)?;
 
-        let mut grouped_events: HashMap<String, Vec<PullRequestEvent>> = HashMap::new();
+        Ok(updates)
+    }
+
+    fn filter_unseen_events(
+        last_seen_timestamps: HashMap<String, DateTime<Utc>>,
+        events: Vec<PullRequestEvent>,
+    ) -> Vec<PullRequestEvent> {
         events
             .into_iter()
-            .filter(|event| match last_seen_timestamps.get(&event.pr_id) {
-                Some(timestamp) => event.timestamp.gt(timestamp),
-                None => true,
+            .filter(|event| {
+                last_seen_timestamps
+                    .get(&event.pr_id)
+                    .map_or(true, |timestamp| event.timestamp.gt(timestamp))
             })
-            .for_each(|event| {
+            .collect()
+    }
+
+    fn group_events_by_pr(events: Vec<PullRequestEvent>) -> HashMap<String, Vec<PullRequestEvent>> {
+        events
+            .into_iter()
+            .fold(HashMap::new(), |mut grouped_events, event| {
                 grouped_events
                     .entry(event.pr_id.clone())
                     .or_insert_with(Vec::new)
-                    .push(event)
-            });
+                    .push(event);
+                grouped_events
+            })
+    }
 
-        // sort by latest event timestamp
-        let mut map_entries: Vec<(String, Vec<PullRequestEvent>)> =
+    fn sort_by_latest_timestamp(
+        grouped_events: HashMap<String, Vec<PullRequestEvent>>,
+    ) -> Vec<(String, Vec<PullRequestEvent>)> {
+        let mut sorted_grouped_events: Vec<(String, Vec<PullRequestEvent>)> =
             grouped_events.into_iter().collect();
-        map_entries.sort_by_key(|(_pr_id, evts)| {
-            evts
-                .iter()
-                .map(|event| event.timestamp)
-                .max()
-                .unwrap_or_default()
-        });
-        map_entries.reverse();
+        sorted_grouped_events
+            .sort_by_key(|(_pr, events)| Reverse(Self::get_latest_timestamp(events)));
+        sorted_grouped_events
+    }
 
-        let updates = map_entries
+    fn get_latest_timestamp(events: &Vec<PullRequestEvent>) -> PullRequestTimestamp {
+        events
+            .iter()
+            .map(|event| event.timestamp)
+            .max()
+            .unwrap_or_default()
+    }
+
+    fn aggregate_events_into_updates(
+        sorted_events: Vec<(String, Vec<PullRequestEvent>)>,
+    ) -> anyhow::Result<Vec<PullRequestUpdate>> {
+        sorted_events
             .into_iter()
             .map(|(pr_id, evts)| aggregate_events(pr_id, evts))
             .collect::<anyhow::Result<Vec<PullRequestUpdate>>>()
-            .context("Could not aggregate events into update.")?;
-
-        Ok(updates)
+            .context("Could not aggregate events into update.")
     }
 }
